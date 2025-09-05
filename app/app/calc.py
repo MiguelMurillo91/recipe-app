1c82abeca9a2ada5e4d95facda5c3a1aa29a8a89
Miguel Murillo <miguelmurillo@Miguels-MacBook-Pro.local>
2025-09-04 22:36:56 -0500

    configured docker compose and checks.yml to wait for DB

---
+++ app/app/calc.py
@@ -0,0 +1,17 @@
+"""
+Function to add 2 numbers
+"""
+
+
+def add(a, b):
+    """
+    Add values a and b, then return the value
+    """
+    return a + b
+
+
+def subtract(a, b):
+    """
+    Subtract values a and b, then return the value
+    """
+    return b - a
